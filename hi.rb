815ecfff996c2cb1ed1c93bf638340531420ec17
Simone <hello@simonewebdesign.it>
2013-10-10 16:49:54 +0200

    Send email

--- hi.rb
+++ hi.rb
@@ -1,4 +1,7 @@
 require 'sinatra'
+require 'pony'
+
+enable :sessions
 
 get '/' do
   erb :index
@@ -9,12 +12,22 @@ get '/about' do
 end
 
 post '/subscribe' do
-  # The HTTP response status code 303 See Other
+  Pony.mail :to => params[:email],
+            :from => "me@example.com",
+            :subject => "Thanks for signing my guestbook!",
+            :html_body => erb(:email, :layout => false),
+            :body => "TODO alternative text for clients who doesn't have HTML"
+
+  # save email to session, for later use
+  session[:email] = params[:email]
+
+  # The HTTP response status code "303 See Other"
   # is the correct way to redirect web applications to a 
   # new URI, particularly after an HTTP POST has been performed.
   redirect '/done', 303
 end
 
 get '/done' do
+  @email = session[:email]
   erb :done
 end
